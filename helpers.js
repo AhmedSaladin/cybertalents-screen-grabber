7a19edd53bd7a510288b27008c5a040d860701fe
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-17 14:12:44 +0200

    Split helper functions

---
+++ helpers.js
@@ -0,0 +1,42 @@
+const cleanupScreen = async (page) => {
+  return await page.evaluate(() => {
+    document.getElementById("stu").style.display = "none";
+    document.getElementsByTagName("footer")[0].style.display = "none";
+    document.getElementsByClassName("go2top")[0].style.display = "none";
+  });
+};
+
+const savechallenge = async (page, url, challengePath) => {
+  await page.goto(url, {
+    waitUntil: "networkidle2",
+  });
+  await cleanupScreen(page);
+  await page.screenshot({
+    path: `${challengePath}challenge.png`,
+    fullPage: true,
+  });
+};
+
+const saveWriteUp = async (page, url, challengePath) => {
+  await page.goto(`${url}/writeups`, {
+    waitUntil: "networkidle2",
+  });
+  await cleanupScreen(page);
+  await page.screenshot({
+    path: `${challengePath}writeup.png`,
+    fullPage: true,
+  });
+};
+const saveLesson = async (page, path) => {
+  await cleanupScreen(page);
+  await page.screenshot({
+    path: `${path}lesson.png`,
+    fullPage: true,
+  });
+};
+
+module.exports = {
+  savechallenge,
+  saveWriteUp,
+  saveLesson,
+};
--- scraper.js
+++ scraper.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const cliProgress = require("cli-progress");
+const { savechallenge, saveWriteUp, saveLesson } = require("./helpers");
 const machine_type = process.platform;
 const fileSeparator = () => {
   return machine_type === "win32" ? "\\" : "/";
@@ -175,44 +176,6 @@ const getPageLessons = async (page, courses, target) => {
   } catch (err) {
     console.log(err);
   }
-};
-
-const cleanupScreen = async (page) => {
-  return await page.evaluate(() => {
-    document.getElementById("stu").style.display = "none";
-    document.getElementsByTagName("footer")[0].style.display = "none";
-    document.getElementsByClassName("go2top")[0].style.display = "none";
-  });
-};
-
-const savechallenge = async (page, url, challengePath) => {
-  await page.goto(url, {
-    waitUntil: "networkidle2",
-  });
-  await cleanupScreen(page);
-  await page.screenshot({
-    path: `${challengePath}challenge.png`,
-    fullPage: true,
-  });
-};
-
-const saveWriteUp = async (page, url, challengePath) => {
-  await page.goto(`${url}/writeups`, {
-    waitUntil: "networkidle2",
-  });
-  await cleanupScreen(page);
-  await page.screenshot({
-    path: `${challengePath}writeup.png`,
-    fullPage: true,
-  });
-};
-
-const saveLesson = async (page, path) => {
-  await cleanupScreen(page);
-  await page.screenshot({
-    path: `${path}lesson.png`,
-    fullPage: true,
-  });
 };
 
 const savePracticeLessons = async (page, course, path) => {
